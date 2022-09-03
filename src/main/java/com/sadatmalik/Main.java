bdef9f03535c38a6e151393e5deaac36f810cdc0
sadat.malik <sadat.malik@me.com>
2022-09-03 11:23:46 +0100

    .gitignore and initial classes

--- src/main/java/com/sadatmalik/Main.java
+++ src/main/java/com/sadatmalik/Main.java
@@ -1,4 +1,6 @@
-package com.sadatmalik;public class Main {
+package com.sadatmalik;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
